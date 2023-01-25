ad18beee48d9d15ff2ccadf7d9208bc8f7e8e455
davit <daviti.gogilashvili.1@btu.edu.ge>
2023-01-25 23:36:57 +0400

    kaiaaaggc

--- app/src/main/java/com/example/myapplication/PassRecover.kt
+++ app/src/main/java/com/example/myapplication/PassRecover.kt
@@ -1,8 +1,56 @@
 package com.example.myapplication
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 
 class PassRecover : Fragment(R.layout.passwordrecover){
+    private lateinit var editText: EditText
+    private lateinit var login : TextView
+    private lateinit var button: Button
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view= inflater.inflate(R.layout.passwordrecover,container,false)
+
+        editText=view.findViewById(R.id.Emailrecover)
+        button=view.findViewById(R.id.button4)
+        login=view.findViewById(R.id.textView10)
+
+        login.setOnClickListener {
+            val fragment1 = LoginFragment()
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.mainContainer, fragment1)
+            transaction?.commit()
+        }
+
+        button.setOnClickListener {
+            if (editText.text.isEmpty()){
+
+            }else{
+                FirebaseAuth.getInstance().sendPasswordResetEmail(editText.text.toString())
+                    .addOnSuccessListener {
+                        Toast.makeText(this.requireContext(), "Check E-mail", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener{
+                        Toast.makeText(this.requireContext(), "Enter the Email you signed up with ", Toast.LENGTH_SHORT).show()
+                    }
+            }
+        }
+
+        return view
+    }
 
 }
 
--- app/src/main/java/com/example/myapplication/ResetPassword.kt
+++ app/src/main/java/com/example/myapplication/ResetPassword.kt
@@ -1,16 +1,26 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
 
-class ResetPassword :Fragment(R.layout.reset_pass) {
+class ResetPassword :Fragment(R.layout.fragment_profilis) {
 
 
     private lateinit var Login : TextView
+    private lateinit var curentpass : EditText
+    private lateinit var newpass: EditText
+    private lateinit var resetpass: Button
+
 
 
     override fun onCreateView(
@@ -18,23 +28,73 @@ class ResetPassword :Fragment(R.layout.reset_pass) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view=inflater.inflate(R.layout.reset_pass,container,false)
+        var view=inflater.inflate(R.layout.fragment_profilis,container,false)
+
+        val preferences = activity?.getSharedPreferences("informacia", Context.MODE_PRIVATE)
+        val editor = preferences?.edit()
         Login=view.findViewById(R.id.Login)
+        curentpass=view.findViewById(R.id.paroli)
+        newpass=view.findViewById(R.id.gaimeore_paroli)
+        resetpass=view.findViewById(R.id.button2)
+
 
         Login.setOnClickListener {
-            val fragment1 =LoginFragment()
+            val fragment1 =xd()
             val transaction = fragmentManager?.beginTransaction()
             transaction?.replace(R.id.mainContainer, fragment1)
             transaction?.commit()
 
         }
+        resetpass.setOnClickListener {
+
+            val curpass = curentpass.text.toString()
+            val nwpass = newpass.text.toString()
+            if (curpass.isNotEmpty() && nwpass.isNotEmpty()) {
+                val user = FirebaseAuth.getInstance().currentUser
+                val credential = EmailAuthProvider.getCredential(user?.email!!, curpass)
+
+                user.reauthenticateAndRetrieveData(credential)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            if (nwpass.length >= 6) {
+                                user?.updatePassword(nwpass)
+                                    ?.addOnCompleteListener { task ->
+                                        if (task.isSuccessful) {
+                                            editor?.remove("password")
+                                            editor?.putString("password", nwpass)
+                                            editor?.apply()
+                                            Toast.makeText(
+                                                this.requireContext(),
+                                                "paroli sheicvala",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+
+                                        } else {
+                                            Toast.makeText(
+                                                this.requireContext(),
+                                                "error",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
+                                    }
+                            }
+                        }else {
+                            Toast.makeText(context, "old password is not correct", Toast.LENGTH_SHORT).show()
+                            curentpass.setText("")
 
 
 
+                        }
+
+                    }
 
 
-
+            }
+            
+        } 
         return view
+
+       
 
     }
 }
