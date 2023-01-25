05672c9965af751734d1ea13fcbe781af74ff4c3
zurabi19 <115415313+zurabi19@users.noreply.github.com>
2023-01-25 01:50:22 -0800

    asdasdaasd

--- app/src/main/java/com/example/myapplication/BookAdapter.kt
+++ app/src/main/java/com/example/myapplication/BookAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-
+import com.example.myapplication.fragments.BookInfoFragment
 
 
 class BookAdapter(private val booklist:ArrayList<Books>)
@@ -21,6 +21,8 @@ class BookAdapter(private val booklist:ArrayList<Books>)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
         val view= LayoutInflater.from(parent.context).inflate(R.layout.items,parent,false)
         return BookViewHolder(view)
+
+
 
     }
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
